e314a642b84646e908426751f1b5b779840c078c
craftyworks <craftyworks@gmail.com>
2020-02-25 21:44:20 +0900

    오늘은 여기까지

--- src/com/ddam40/kia/chapter7/Chapter7.kt
+++ src/com/ddam40/kia/chapter7/Chapter7.kt
@@ -1,2 +1,34 @@
 package com.ddam40.kia.chapter7
 
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y)
+    }
+}
+
+operator fun Point.times(scale: Double) = Point((x * scale).toInt(), (y * scale).toInt())
+
+operator fun Char.times(count: Int): String {
+    return toString().repeat(count)
+}
+
+fun main() {
+    val p0 = Point(3, 3)
+    val p1 = Point(4, 4)
+
+    val p2 = p0.plus(p1)
+    val p3 = p0 + p1
+
+    println("point : $p2, $p3")
+
+    println("point * 3 = ${p0 * 3.0}")
+
+    println('a' * 5)
+
+    println(5 * 'a')
+}
+
+private operator fun Int.times(c: Char): String {
+    return c.toString().repeat(this)
+}
+
